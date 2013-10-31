7ad34d0ef02c85bdfd58f09526cfc9395279dd92
guyannanfei25 <1913744842@qq.com>
2013-10-31 17:25:11 +0800

    add
    增加c语言按要求分割字符串和读配置文件的源码

---
+++ c语言分割函数.c
@@ -0,0 +1,21 @@
+﻿#include<stdio.h>
+#include<string.h>
+#define SIZE 10
+int my_split(char *p[SIZE], char str[],char const *sep)
+{
+	int num = 0;
+	for(p[num] = strtok(str,sep); p[num] != NULL; p[++num] = strtok(NULL,sep));
+	return num;//返回被分割成单独部分的个数
+}
+int main( )
+{
+	char line[] = "split@string/to#many single,part]ok";
+	char *p[SIZE];
+	char const *token = " ,/@#]";
+	int num = my_split( p, line,token);
+	for(int i = 0; i < num; i ++ )
+	{
+		printf("%s\n",p[i]);
+	}
+	return 0;
+}
