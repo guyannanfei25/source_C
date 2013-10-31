5e10c3d511c022c6c64cbc13ab6168c1c4d7008d
guyannanfei25 <1913744842@qq.com>
2013-10-31 17:32:36 +0800

    add
    增加根据空格分隔字符串小程序

---
+++ 根据空格分割字符串.c
@@ -0,0 +1,12 @@
+﻿#include <string.h>
+#include <stdio.h>
+int main(void)
+{
+	char input[16] = "abc,d";
+	char *p;
+	p = strtok(input, ",");
+	if (p) printf("%s\n", p);
+	p = strtok(NULL, ",");
+	if (p) printf("%s\n", p);
+	return 0;
+}
